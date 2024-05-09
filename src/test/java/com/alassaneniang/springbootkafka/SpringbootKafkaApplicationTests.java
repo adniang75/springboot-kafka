2004c5c308b5f2e48e20924c58ffedd2e52f630c
adniang75 <adniang75@gmail.com>
2024-05-09 23:37:23 +0200

    Initial Commit

---
+++ src/test/java/com/alassaneniang/springbootkafka/SpringbootKafkaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.alassaneniang.springbootkafka;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@DisplayName("Application Context Test")
+class SpringbootKafkaApplicationTests {
+
+    @Test
+    @DisplayName("Context Loading")
+    void contextLoads() {
+    }
+
+}
